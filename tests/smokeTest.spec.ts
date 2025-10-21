ba4fc39c7c43d14eca0e0da795b2d6451b8af8bf
alledanse <andrew.alledanse@gmail.com>
2025-10-21 15:27:03 +0300

    added test data generation

--- tests/smokeTest.spec.ts
+++ tests/smokeTest.spec.ts
@@ -2,6 +2,8 @@ import { expect } from '../utils/custom-expect';
 import { test } from '../utils/fixtures';
 import { validateSchema } from '../utils/schemaValidator';
 import articleRequestPayload from '../request-objects/POST-article.json';
+import { faker } from '@faker-js/faker';
+import { getNewRandomArticle } from '../utils/dataGenerator';
 
 test('Get articles', async ({ api }) => {
   const response = await api
@@ -21,18 +23,17 @@ test('Get test tags', async ({ api }) => {
   expect(response.tags.length).toBeLessThanOrEqual(10);
 });
 test('Create and delete article', async ({ api }) => {
-  const articleRequest = JSON.parse(JSON.stringify(articleRequestPayload));
+  const articleRequest = getNewRandomArticle();
   const createArticleResponse = await api.path('/articles').body(articleRequest).postRequest(201);
   await expect(createArticleResponse).shouldMatchSchema('articles', 'POST_articles');
-
-  expect(createArticleResponse.article.title).shouldEqual('Test TWO TEST');
+  expect(createArticleResponse.article.title).shouldEqual(articleRequest.article.title);
   const slugId = createArticleResponse.article.slug;
 
   const articleResponse = await api
     .path('/articles')
     .params({ limit: 10, offset: 0 })
     .getRequest(200);
-  expect(articleResponse.articles[0].title).shouldEqual('Test TWO TEST');
+  expect(articleResponse.articles[0].title).shouldEqual(articleRequest.article.title);
 
   await api.path(`/articles/${slugId}`).deleteRequest(204);
 
@@ -40,40 +41,33 @@ test('Create and delete article', async ({ api }) => {
     .path('/articles')
     .params({ limit: 10, offset: 0 })
     .getRequest(200);
-  expect(articleResponseTwo.articles[0].title).not.shouldEqual('Test TWO TEST');
+  expect(articleResponseTwo.articles[0].title).not.shouldEqual(articleRequest.article.title);
 });
 
 test('Create, update and delete article', async ({ api }) => {
-  const createArticleResponse = await api
-    .path('/articles')
+  const articleTitle = faker.lorem.sentence(5);
+  const articleRequest = JSON.parse(JSON.stringify(articleRequestPayload));
+  articleRequest.article.title = articleTitle;
+  const createArticleResponse = await api.path('/articles').body(articleRequest).postRequest(201);
 
-    .body({
-      article: {
-        title: 'Test TWO TEST',
-        description: 'Test description',
-        body: 'Test body',
-        tagList: [],
-      },
-    })
-    .postRequest(201);
-
-  expect(createArticleResponse.article.title).shouldEqual('Test TWO TEST');
+  expect(createArticleResponse.article.title).shouldEqual(articleTitle);
   const slugId = createArticleResponse.article.slug;
 
-  articleRequestPayload.article.title = 'Test new article modified';
+  const articleTitleModified = faker.lorem.sentence(5);
+  articleRequest.article.title = articleTitleModified;
   const updateArticleResponse = await api
     .path(`/articles/${slugId}`)
-    .body(articleRequestPayload)
+    .body(articleRequest)
     .putRequest(200);
 
-  expect(updateArticleResponse.article.title).shouldEqual('Test new article modified');
+  expect(updateArticleResponse.article.title).shouldEqual(articleTitleModified);
   const newSlugId = updateArticleResponse.article.slug;
 
   const articleResponse = await api
     .path('/articles')
     .params({ limit: 10, offset: 0 })
     .getRequest(200);
-  expect(articleResponse.articles[0].title).shouldEqual('Test new article modified');
+  expect(articleResponse.articles[0].title).shouldEqual(articleTitleModified);
 
   await api.path(`/articles/${newSlugId}`).deleteRequest(204);
 
@@ -81,5 +75,5 @@ test('Create, update and delete article', async ({ api }) => {
     .path('/articles')
     .params({ limit: 10, offset: 0 })
     .getRequest(200);
-  expect(articleResponseTwo.articles[0].title).not.shouldEqual('Test new article modified');
+  expect(articleResponseTwo.articles[0].title).not.shouldEqual(articleTitleModified);
 });
---
+++ utils/dataGenerator.ts
@@ -0,0 +1,11 @@
+import articleRequestPayload from '../request-objects/POST-article.json';
+import { faker } from '@faker-js/faker';
+
+export function getNewRandomArticle() {
+  //   const articleRequest = JSON.parse(JSON.stringify(articleRequestPayload));
+  const articleRequest = structuredClone(articleRequestPayload);
+  articleRequest.article.title = faker.lorem.sentence(5);
+  articleRequest.article.description = faker.lorem.sentence(3);
+  articleRequest.article.body = faker.lorem.paragraph(8);
+  return articleRequest;
+}
